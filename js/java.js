f9e5528330264bf95e90827e7c0c3ee226b58d06
Dominic <125dmiguelcruz@frhsd.com>
2024-06-05 09:47:06 -0400

    butn1

---
+++ js/java.js
@@ -0,0 +1,60 @@
+function updateOne(){
+    document.getElementById('but1').style.color = "#D1321F";
+}
+function updateTwo(){
+    document.getElementById('but2').style.color = "#D1321F";
+}
+function updateThree(){
+    document.getElementById('but3').style.color = "#D1321F";
+}
+function updateFour(){
+    document.getElementById('but4').style.color = "#628322";
+}
+function updateFive(){
+    document.getElementById('but5').style.color = "#D1321F";
+}
+function updateSix(){
+    document.getElementById('but6').style.color = "#D1321F";
+}
+function updateSeven(){
+    document.getElementById('but7').style.color = "#628322";
+}
+function updateEight(){
+    document.getElementById('but8').style.color = "#D1321F";
+}
+function updateNine(){
+    document.getElementById('but9').style.color = "#D1321F";
+}
+function updateTen(){
+    document.getElementById('but10').style.color = "#D1321F";
+}
+function updateEleven(){
+    document.getElementById('but11').style.color = "#D1321F";
+}
+function updateTwelve(){
+    document.getElementById('but12').style.color = "#628322";
+}
+function updateThirteen(){
+    document.getElementById('but13').style.color = "#D1321F";
+}
+function updateFourteen(){
+    document.getElementById('but14').style.color = "#D1321F";
+}
+function updateFifthteen(){
+    document.getElementById('but15').style.color = "#628322";
+}
+function updateSixteen(){
+    document.getElementById('but16').style.color = "#D1321F";
+}
+function updateSeventeen(){
+    document.getElementById('but17').style.color = "#D1321F";
+}
+function updateEighteen(){
+    document.getElementById('but18').style.color = "#D1321F";
+}
+function updateNineteen(){
+    document.getElementById('but19').style.color = "#628322";
+}
+function updateTwenty(){
+    document.getElementById('but20').style.color = "#D1321F";
+}
