dc28bf5603c972c6540975f1359bc93523b07edd
Dominic <125dmiguelcruz@frhsd.com>
2024-06-06 10:41:03 -0400

    buttons

--- js/java.js
+++ js/java.js
@@ -58,3 +58,63 @@ function updateNineteen(){
 function updateTwenty(){
     document.getElementById('but20').style.color = "#D1321F";
 }
+function updateEventeen(){
+    document.getElementById('but21').style.color = "#D1321F";
+}
+function updateIghteen(){
+    document.getElementById('but22').style.color = "#D1321F";
+}
+function updateIneteen(){
+    document.getElementById('but23').style.color = "#628322";
+}
+function updateWenty(){
+    document.getElementById('but24').style.color = "#D1321F";
+}
+function updateOnee(){
+    document.getElementById('but25').style.color = "#D1321F";
+}
+function updateTwoo(){
+    document.getElementById('but26').style.color = "#D1321F";
+}
+function updateThreee(){
+    document.getElementById('but27').style.color = "#D1321F";
+}
+function updateFourr(){
+    document.getElementById('but28').style.color = "#628322";
+}
+function updateOneee(){
+    document.getElementById('but29').style.color = "#D1321F";
+}
+function updateTtwooo(){
+    document.getElementById('but30').style.color = "#628322";
+}
+function updateThreeee(){
+    document.getElementById('but31').style.color = "#D1321F";
+}
+function updateFourrr(){
+    document.getElementById('but32').style.color = "#D1321F";
+}
+function updateOneeee(){
+    document.getElementById('but33').style.color = "#628322";
+}
+function updateTwooo(){
+    document.getElementById('but34').style.color = "#D1321F";
+}
+function updateThreeeee(){
+    document.getElementById('but35').style.color = "#D1321F";
+}
+function updateFourrrr(){
+    document.getElementById('but36').style.color = "#D1321F";
+}
+function updateOone(){
+    document.getElementById('but37').style.color = "#D1321F";
+}
+function updateTtwo(){
+    document.getElementById('but38').style.color = "#D1321F";
+}
+function updateTthree(){
+    document.getElementById('but39').style.color = "#D1321F";
+}
+function updateFfour(){
+    document.getElementById('but40').style.color = "#628322";
+}
